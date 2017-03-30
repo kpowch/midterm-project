e2ecaa75f90473387ef531c43c7bec15ce5be8ed
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-03-30 22:22:57 +0000

    incorporates database user data into /editprofile route

--- server/routes/users.js
+++ server/routes/users.js
@@ -18,7 +18,6 @@ module.exports = (knex) => {
 
   //TODO need to get username, email, and all things that they like/own
   router.get("/:user_id", (req, res) => {
-
     var templateVars = {
       user: {
         name: 'name',
@@ -29,14 +28,26 @@ module.exports = (knex) => {
 
   });
 
+  // need username, email, password from db
   router.get("/:user_id/editprofile", (req, res) => {
-    var templateVars = {
-      user: {
-        name: 'name',
-        email: 'user@email.com'
-      }
-    };
-    res.render('../public/views/users_user_id_editprofile', templateVars)
+    knex
+      .select('*')
+      .from('users')
+      .where('id', 1)
+      .then((results) => {
+        // console.log(results[0].id);
+        var templateVars = {
+          user: {
+            id: results[0].id,
+            username: results[0].username,
+            email: results[0].email,
+            password: results[0].password
+          }
+        }
+        console.log(templateVars);
+        res.render('../public/views/users_user_id_editprofile', templateVars)
+      })
+
 
   });
 
