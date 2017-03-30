c85fd4ffb0a8acfef7735dc904067f6c1b09cf02
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-03-30 23:47:24 +0000

    renders all resources on index page!

--- server/server.js
+++ server/server.js
@@ -46,51 +46,16 @@ app.use("/resources", resourceRoutes(knex));
 // Home page
 // from db, need username, all resources
 app.get("/", (req, res) => {
-  // knex
-  //   .select('*')
-  //   .from('users')
-  //   .where('id', 3)
-  //   .then((results) => {
-  //     // console.log(results[0].id);
-  //     var templateVars = {
-  //       user: {
-  //         id: results[0].id,
-  //         username: results[0].username,
-  //         email: results[0].email,
-  //       }
-  //     }
-
   knex('resources')
     .join('users', 'users.id', '=', 'resources.creator')
     .then((results) => {
-        console.log(results);
-        // for (var resource in results) {
-        //   var templateVars = {
-        //     user: {
-        //       username: results[resource].username
-        //     },
-        //     resources: {
-        //       id: results[resource].id,
-        //       title: results[resource].title,
-        //       url: results[resource].url,
-        //       description: results[resource].description,
-        //       topic: results[resource].topic,
-        //       creator: results[resource].username,
-        //       date_created: results[resource].date_created
-        //     }
-        //   }
-        var templateVars = {
-            user: {
-              username: 'to be changed'
-            },
-            resources: results
-          }
-
-        res.render("../public/views/index", templateVars);
-
-        //   console.log(templateVars)
-
-
+      var templateVars = {
+          user: {
+            username: 'to be changed'
+          },
+          resources: results
+      }
+      res.render("../public/views/index", templateVars);
   });
 });
 
