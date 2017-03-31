b0444913faf4999f28066f74690d88fe3919c48a
Brendan Walker <brenny_walker@hotmail.com>
2017-03-31 01:46:28 +0000

    Adds resource and creator to unique resource page

--- server/routes/resources.js
+++ server/routes/resources.js
@@ -4,16 +4,6 @@ const express = require('express');
 const router  = express.Router();
 const fs = require('fs');
 
-// const db = [{
-//   "title": "subway",
-//   "url": "www.shubway.com",
-//   "description": "Tutorial on subway",
-//   "topic": "food",
-//   "creator": "4",
-//   "date_created": "10/23/2017"
-// }]
-
-let fakeUserCookie = 1;
 
 module.exports = (knex) => {
 
@@ -29,10 +19,13 @@ module.exports = (knex) => {
 
   router.get("/:resource_id", (req, res) => {
     let key = req.params.resource_id;
-    let resource;
-    let creatorName = {};
-    //knex.select().from('users').where('id', fakeUserCookie)
+    let resource = {};
+    let creatorName = "";
+    let likes = 0;
+    let ratings = 0;
+    let comments = {};
 
+    //links user with resource
     knex('users').join('resources', 'users.id', '=', 'creator')
     .select('username').where('resources.id', key)
     .asCallback((err, results) => {
@@ -40,6 +33,21 @@ module.exports = (knex) => {
       creatorName = results[0].username;
     });
 
+    //links resource likes
+    // knex('resources').join('likes', 'resource_id', '=', 'resources.id')
+    // .count().where('resource_id', key)
+    // .asCallback((err, results) => {
+    //   if (err) return console.error(err);
+    //   likes = results[0].count;
+    // });
+
+
+
+
+
+
+
+    //retrieves resource from database
     knex.select().from('resources').where('id', key)
     .asCallback((err, results) => {
       if (err) return console.error(err);
@@ -49,17 +57,25 @@ module.exports = (knex) => {
 
       let templateVars = {
         resource: resource[0],
-        user: {
+        creator: {
           username: creatorName
+        },
+        user: {
+          username: 'Brendan'
         }
+        // likesAmount: {
+        //   likes: likes
+        // }
       }
-      //console.log('templateVars', templateVars);
+      console.log('templateVars', templateVars);
       res.render("../public/views/resource_id", templateVars);
     }).catch(function(error){
         console.log(error);
     })
 
   });
+
+
 
   router.post("/create", (req, res) => {
     //receive resource from req.body
