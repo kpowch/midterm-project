04ff8965a998441bf05d6d815ddd24d10c6668f3
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-01 23:34:16 +0000

    adds search field functionality!

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,25 +1,19 @@
-// these are routes that return a json
-
 "use strict";
+//TODO look into what knex.raw is since it might prevent injection attacks??
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (knex) => {
-
-  // for all resources
-  router.get('/resources', (req, res) => {
-
+  /*
+  This is to filter by topic on the home page (all resources) when the user
+  selects from the list of topics. The topics come as an array.
+  */
+  router.get('/resources/filter', (req, res) => {
     // the search/filters will come as req.query
-    console.log('Req query coming through api:', req.query);
-
-    let filterObj = req.query;
-
-// console.log('filterobj', filterObj, filterObj.topic.length)
+    const filterObj = req.query;
 
     if (filterObj.topic.length) {
-      // need to make this modular so they can add in more search/filter parameters
-      // TODO this query is totes wrong btw
       knex('resources')
       .whereIn('topic', filterObj.topic)
         .then((results) => {
@@ -27,27 +21,37 @@ module.exports = (knex) => {
       }).catch((error) => {
         console.log(error);
       });
-    } else {
-      knex('resources')
-        .then((results) => {
-          res.json(results);
-        }).catch((error) => {
-        console.log(error);
-      });
     }
   });
 
-  // for my resources
-  router.get('/users/:user_id/resources', (req, res) => {
-    // this does the same as the above but user specific (and likes table)
-    console.log('Req query coming through api:', req.query);
+  /*
+  This is to filter by topic on the home page (all resources) when the user
+  types something in the search bar.
+  */
+  //TODO make sure compared as both uppercase
+  router.get('/resources/search', (req, res) => {
+    const searchQuery = req.query.search.toLowerCase();
+    knex('resources')
+      .whereRaw("LOWER(title) LIKE '%' || LOWER(?) || '%' ", searchQuery)
+      .orWhereRaw("LOWER(description) LIKE '%' || LOWER(?) || '%' ", searchQuery)
+      .then((results) => {
+        res.json(results);
+      }).catch((error) => {
+        console.log(error);
+      })
+  })
 
-    let filterObj = req.query;
-    let userId = req.params.user_id;
+  /*
+  This is to filter by topic on the users resources page when the user
+  selects from the list of topics. The topics come as an array.
+  */
+  router.get('/users/:user_id/resources/filter', (req, res) => {
+    // this does the same as the above but user specific (and likes table)
+    const filterObj = req.query;
+    const userId = req.params.user_id;
     let subQueryLikes = knex('likes').select('resource_id').where('user_id', userId);
 
     if (filterObj.topic.length) {
-
       knex('resources')
         .whereIn('topic', filterObj.topic)
         .where(function() {
@@ -59,15 +63,34 @@ module.exports = (knex) => {
       }).catch((error) => {
         console.log(error);
       });
-    } else {
-      knex('resources')
-        .then((results) => {
-          res.json(results);
-        }).catch((error) => {
-        console.log(error);
-      });
     }
   });
+
+  /*
+  This is to filter by topic on the home page (all resources) when the user
+  types something in the search bar.
+  */
+  router.get('/users/:user_id/resources/search', (req, res) => {
+    // this does the same as the above but user specific (and likes table)
+    const searchQuery = req.query.search.toLowerCase();
+    const userId = req.params.user_id;
+    let subQueryLikes = knex('likes').select('resource_id').where('user_id', userId);
+
+    knex('resources')
+      .where(function() {
+        this.whereRaw("LOWER(title) LIKE '%' || LOWER(?) || '%' ", searchQuery)
+          .orWhereRaw("LOWER(description) LIKE '%' || LOWER(?) || '%' ", searchQuery)
+      })
+      .where(function() {
+        this.where('creator', userId)
+        .orWhereIn('id', subQueryLikes)
+    })
+    .then((results) => {
+      res.json(results);
+    }).catch((error) => {
+      console.log(error);
+    });
+  })
 
   return router;
 }
