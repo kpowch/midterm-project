9d9f08713873078252d0a071cfbe25c960f3dec3
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-02 18:08:13 +0000

    adds start to middleware

--- server/routes/middleware.js
+++ server/routes/middleware.js
@@ -1,45 +1,30 @@
 const express = require('express');
-const ENV         = process.env.ENV || "development";
-const knexConfig  = require("../knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
 
-module.exports = () => {
+module.exports = (knex) => {
   // to forbid someone from seeing the page/post
-  ensureLoggedIn: function(req, res, next) {
+  const ensureLoggedIn = function (req, res, next) {
 
-    let currentUser = '';
-
-    //checks for a logged in user, if no cookie-session, currentUser an empty string
-    if(req.session.user_id === undefined) {
-      let templateVars = {
-          user: {
-            username: currentUser
-          }
-        }
-        res.render(templateVars);
-        return;
-      } else {
-        knex('users').select('username').where('users.id', req.session.user_id)
-        .asCallback((err, results) => {
-        if (err) console.error(err);
-
-        if (results[0].username.length > 0) {
-        currentUser = results[0].username;
-        let templateVars = {
-          user: {
-            username: currentUser,
-            userID: req.session.user_id
-          }
-        }
-        res.render(templateVars);
-        return;
-        }
-      });
+    if (!req.session.user_id) {
+      return res.sendStatus(403);
     }
-    // if not logged in, redirect to error page? alert they need to log in? redirect to login?
-    // potential, save their url and once they're logged in, redirect to that url
 
+    next();
   }
-  // TODO make another mmiddleware
-  return ensureLoggedIn;
+
+  // TODO this is how we extract logged in user and pass it to routes. in route, call req.username
+  const extractUserData = function (req, res, next) {
+    knex('users').where({ id: req.session.user_id }).then(users => {
+      if (!users.length) {
+        return next()
+      }
+      req.username = users[0].name
+      return next()
+    })
+  }
+
+  return {
+    ensureLoggedIn,
+    extractUserData
+  };
+
 }
