cc3634cc35d7551b694b75630112d10c2f0ef8b6
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-03 00:16:56 +0000

    moves routes in server.js to pages.js

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -1,16 +1,139 @@
 "use strict";
-//TODO add other routes in here so server.js is cleaner
 
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 module.exports = (knex) => {
+  // Home page
+  router.get('/', (req, res) => {
+    // this is used for the initial page render
+    // TODO add username for header
+    let currentUser = '';
+    if (!req.session.user_id) {
+      console.log(req.session.user_id);
+      console.log('no cookie');
+      knex('resources')
+      .then((rows) => {
+        res.render('../public/views/index', { user: undefined, resources: rows });
+        return;
+      });
+    } else {
+      console.log(req.session.user_id);
+      knex('users').select('username').where('users.id', req.session.user_id)
+      .asCallback((err, results) => {
+        if (err) throw err;
+        console.log(results[0]);
+        if (results[0].username.length > 0) {
+          currentUser = results[0].username;
+          let ID = req.session.user_id;
+          knex('resources')
+          .then((rows) => {
+          res.render('../public/views/index', { user: {username: currentUser, userID: ID}, resources: rows });
+          return;
+          });
+        }
+      });
+    }
+  });
+
+  router.get('/login', (req, res) => {
+      res.render('../public/views/login', {
+      errors: req.flash('errors'),
+      info: req.flash('info')
+    });
+    req.session = null;
+  });
+
+  router.post('/login', (req, res) => {
+    const findReqEmail = knex('users')
+    .select('id', 'password')
+    .where({email: req.body.email_login})
+    .limit(1);
+    findReqEmail.then((results) => {
+      const user = results[0];
+      if (!user) {
+        return Promise.reject({
+          type: 409,
+          message: 'Bad credentials'
+        });
+      }
+      const comparePasswords = bcrypt.compare(req.body.password_login, user.password);
+      return comparePasswords.then((passwordsMatch) => {
+      if (!passwordsMatch) {
+        return Promise.reject({
+          type: 409,
+          message: 'Bad credentials'
+        });
+      }
+      return Promise.resolve(user);
+      });
+    }).then((user) => {
+      req.session.user_id = user.id;
+      res.redirect('/');
+    }).catch((err) => {
+      req.flash('errors', err.message);
+      res.redirect('/login');
+    });
+  });
+
+
+  router.post('/register', (req, res) => {
+
+    if (!req.body.email_register || !req.body.password_register || !req.body.username_register) {
+      req.flash('errors', 'Email, password, and username required');
+      res.redirect('/login');
+      return;
+    }
+    const findReqEmailUsername = knex('users')
+    .select(1)
+    .where({email: req.body.email_register})
+    .orWhere({username: req.body.username_register})
+    .limit(1);
+
+    findReqEmailUsername.then((results) => {
+      if (results.length) {
+        return Promise.reject({
+          type: 409,
+          message: 'Email or username already used'
+        });
+      }
+      return bcrypt.hash(req.body.password_register, 10);
+    }).then((passwordDigest) => {
+      return knex('users').insert({
+        email: req.body.email_register,
+        username: req.body.username_register,
+        password: passwordDigest
+      });
+    }).then(() => {
+      req.flash('info', 'Account successfully created, please log in');
+      knex('users').select('users.id')
+      .where('users.email', req.body.email_register)
+      .then((results) => {
+        req.session.user_id = results[0].id;
+        console.log('register cookie', results[0]);
+        res.redirect('/');
+        return;
+      });
+    }).catch((err) => {
+      req.flash('errors', err.message);
+      res.redirect('/login');
+      return;
+    });
+  });
+
+  //deletes session cookie, logs out, and redirects to home page
+  router.post('/logout', (req, res) => {
+    console.log(req.session);
+    req.session = null;
+    res.redirect('/');
+  });
+
+  //see if a topic is picked
+  router.post('/topics', (req, res) => {
+    console.log(req.body);
+  })
 
   // put all other routes in here
   return router;
 }
-
-
-// add this to server:
-// const pagesRoutes = require("./routes/pages");
-// app.use('/', pagesRoutes(knex));
--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
-"use strict";
+'use strict';
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
-const bcrypt      = require("bcrypt");
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || 'development';
+const express = require('express');
+const bodyParser = require('body-parser');
+const sass = require('node-sass-middleware');
+const app = express();
 
 const cookieSession = require('cookie-session');
 app.use(cookieSession({
@@ -16,175 +15,45 @@ app.use(cookieSession({
   keys: [process.env.SESSION_SECRET || 'development']
 }));
 
-const knexConfig  = require("../knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+const knexConfig = require('../knexfile');
+const knex = require('knex')(knexConfig[ENV]);
+const morgan = require('morgan');
+const knexLogger = require('knex-logger');
 
-const flash       = require("connect-flash");
+const flash = require('connect-flash');
 app.use(flash());
 
-// Middleware to check if logged in
-const middleware = require("./routes/middleware")(knex);
-// Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
-const resourceRoutes = require("./routes/resources");
-const apiRoutes = require("./routes/api");
+// Middleware to check if logged in and to return username of logged in user
+const middleware = require('./routes/middleware')(knex);
+const usersRoutes = require('./routes/users');
+const resourceRoutes = require('./routes/resources');
+const apiRoutes = require('./routes/api');
+const pagesRoutes = require('./routes/pages');
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+//   The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("../public/styles/", sass({
-  src: __dirname + "../public/styles",
-  dest: __dirname + "../public/styles",
+app.use('../public/styles/', sass({
+  src: __dirname + '../public/styles',
+  dest: __dirname + '../public/styles',
   debug: true,
   outputStyle: 'expanded'
 }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Mount all resource routes
-app.use("/users", middleware.extractUserData, usersRoutes(knex));
-//TODO if we want to forbid certain routes for resoruces (not all) , then put only on the forbidden ones
-app.use("/resources", middleware.extractUserData, resourceRoutes(knex));
+app.use('/users', middleware.extractUserData, usersRoutes(knex));
+app.use('/resources', middleware.extractUserData, resourceRoutes(knex));
 app.use('/api', apiRoutes(knex));
-
-// Home page
-// from db, need username, all resources
-app.get("/", (req, res) => {
-  // this is used for the initial page render
-  // TODO add username for header
-  let currentUser = '';
-  if (!req.session.user_id) {
-    console.log(req.session.user_id);
-    console.log('no cookie');
-    knex('resources')
-    .then((rows) => {
-      res.render("../public/views/index", { user: undefined, resources: rows });
-      return;
-    });
-  } else {
-    console.log(req.session.user_id);
-    knex('users').select('username').where('users.id', req.session.user_id)
-    .asCallback((err, results) => {
-      if (err) throw err;
-      console.log(results[0]);
-      if (results[0].username.length > 0) {
-        currentUser = results[0].username;
-        let ID = req.session.user_id;
-        knex('resources')
-        .then((rows) => {
-        res.render("../public/views/index", { user: {username: currentUser, userID: ID}, resources: rows });
-        return;
-        });
-      }
-    });
-  }
-});
-
-app.get("/login", (req, res) => {
-    res.render("../public/views/login", {
-    errors: req.flash('errors'),
-    info: req.flash('info')
-  });
-  req.session = null;
-});
-
-app.post("/login", (req, res) => {
-  const findReqEmail = knex('users')
-  .select('id', 'password')
-  .where({email: req.body.email_login})
-  .limit(1);
-  findReqEmail.then((results) => {
-    const user = results[0];
-    if (!user) {
-      return Promise.reject({
-        type: 409,
-        message: 'Bad credentials'
-      });
-    }
-    const comparePasswords = bcrypt.compare(req.body.password_login, user.password);
-    return comparePasswords.then((passwordsMatch) => {
-    if (!passwordsMatch) {
-      return Promise.reject({
-        type: 409,
-        message: 'Bad credentials'
-      });
-    }
-    return Promise.resolve(user);
-    });
-  }).then((user) => {
-    req.session.user_id = user.id;
-    res.redirect('/');
-  }).catch((err) => {
-    req.flash('errors', err.message);
-    res.redirect('/login');
-  });
-});
-
-
-app.post("/register", (req, res) => {
-
-  if (!req.body.email_register || !req.body.password_register || !req.body.username_register) {
-    req.flash('errors', 'Email, password, and username required');
-    res.redirect('/login');
-    return;
-  }
-  const findReqEmailUsername = knex('users')
-  .select(1)
-  .where({email: req.body.email_register})
-  .orWhere({username: req.body.username_register})
-  .limit(1);
-
-  findReqEmailUsername.then((results) => {
-    if (results.length) {
-      return Promise.reject({
-        type: 409,
-        message: 'Email or username already used'
-      });
-    }
-    return bcrypt.hash(req.body.password_register, 10);
-  }).then((passwordDigest) => {
-    return knex('users').insert({
-      email: req.body.email_register,
-      username: req.body.username_register,
-      password: passwordDigest
-    });
-  }).then(() => {
-    req.flash('info', 'Account successfully created, please log in');
-    knex('users').select('users.id')
-    .where('users.email', req.body.email_register)
-    .then((results) => {
-      req.session.user_id = results[0].id;
-      console.log('register cookie', results[0]);
-      res.redirect('/');
-      return;
-    });
-  }).catch((err) => {
-    req.flash('errors', err.message);
-    res.redirect('/login');
-    return;
-  });
-});
-
-//deletes session cookie, logs out, and redirects to home page
-app.post("/logout", (req, res) => {
-  console.log(req.session);
-  req.session = null;
-  res.redirect('/');
-});
-
-//see if a topic is picked
-app.post("/topics", (req, res) => {
-  console.log(req.body);
-})
+app.use('/', pagesRoutes(knex));
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log('Example app listening on port ' + PORT);
 });
